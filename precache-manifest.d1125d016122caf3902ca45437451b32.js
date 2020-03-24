f57d8775b0cb6d14ac661dd0c3b7c404035a31ac
drake0770 <n.pyhlyuk@gmail/com>
2020-03-24 11:22:33 +0200

    Updates

---
+++ precache-manifest.d1125d016122caf3902ca45437451b32.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6a1ceb7fe0b352a5612afb3d9f5925c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "595c2e0d06136cd778a2",
+    "url": "/static/css/main.5d151010.chunk.css"
+  },
+  {
+    "revision": "8856932d97f0c5959908",
+    "url": "/static/js/2.35def98a.chunk.js"
+  },
+  {
+    "revision": "2d1f0ed21c5c95e3a2cb212dbd2653e2",
+    "url": "/static/js/2.35def98a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "595c2e0d06136cd778a2",
+    "url": "/static/js/main.9ad0f612.chunk.js"
+  },
+  {
+    "revision": "83f86c744e8198fd84c5",
+    "url": "/static/js/runtime-main.dfea2bb0.js"
+  },
+  {
+    "revision": "7fa5e4d7b746e1e519c710653c78e954",
+    "url": "/static/media/airpods.7fa5e4d7.jpg"
+  },
+  {
+    "revision": "11ab56c57ac53ff56dae0a7510388d45",
+    "url": "/static/media/airpodspro.11ab56c5.jpg"
+  },
+  {
+    "revision": "5aa41adff2769192455a3c648bb9f9dc",
+    "url": "/static/media/imaccustom.5aa41adf.jpg"
+  },
+  {
+    "revision": "9470fa9b75023290ce3e5b24a5b92c34",
+    "url": "/static/media/ipad19.9470fa9b.jpg"
+  },
+  {
+    "revision": "1d8368b3536a6ef3e8210b033353c657",
+    "url": "/static/media/ipadpro.1d8368b3.jpg"
+  },
+  {
+    "revision": "f2071ac608f2723343d4a974b2477701",
+    "url": "/static/media/iphone11.f2071ac6.jpeg"
+  },
+  {
+    "revision": "7f6a7a27d0813d66d58b52a8202606e2",
+    "url": "/static/media/iphone7.7f6a7a27.jpg"
+  },
+  {
+    "revision": "1d36079df6cf0b3116af79f178db5e77",
+    "url": "/static/media/iphone7plus.1d36079d.jpg"
+  },
+  {
+    "revision": "259c010e0495f2cf2c98e0dbbf4020e9",
+    "url": "/static/media/iphone8.259c010e.jpeg"
+  },
+  {
+    "revision": "31dadabf796c4935e93ebbd57084ed1f",
+    "url": "/static/media/iphonex.31dadabf.jpeg"
+  },
+  {
+    "revision": "7959663d35fb02316a91a753ccb1d601",
+    "url": "/static/media/macpro.7959663d.jpg"
+  },
+  {
+    "revision": "157a06b0e22c8edaed70916eafa53a83",
+    "url": "/static/media/usbcable.157a06b0.jpg"
+  },
+  {
+    "revision": "84a58b5bf7d424f96f1636eaf347b0cf",
+    "url": "/static/media/watch3.84a58b5b.jpg"
+  }
+]);
